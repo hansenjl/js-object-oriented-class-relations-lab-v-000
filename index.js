d83468d7fe0608ccf722aa3de4b89f48fdb609c1
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-07-30 18:38:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,49 @@
+let store = {drivers: [], passengers:[], trips:[]}
+
+let driverId = 0
+let passengerId = 0
+let tripId = 0
+
+class Driver{
+  constructor(name){
+    this.id = ++driverId
+    this.name = name
+    store.drivers.push(this)
+  }
+}
+
+class Passenger{
+  constructor(name){
+    this.id = ++passengerId
+    this.name = name
+    store.passengers.push(this)
+  }
+  
+}
+
+//join table model 
+class Trip{
+  constructor(driver,passenger){
+    this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    store.trips.push(this)
+  }
+  
+  driver(){
+    return store.drivers.find(function(driver){
+      console.log(this)
+      return driver.id === this.driverId
+    })
+  }
+  
+  
+  setPassenger(passenger){
+    this.passengerId = passenger.id
+  }
+  passenger(){
+    return store.passengers.find(function(passenger){
+      return passenger.id === this.passengerId
+    })
+  }
+}
